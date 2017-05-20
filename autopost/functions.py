2455ab5b2c04ab12e8c17a4641c3614e7fbf04e6
b2r2 <b2r2@bk.ru>
2017-05-20 18:24:56 +0300

    add file fucntion.py and edit file main.py(transposition fucntion log() form main in fucntion and delete spare code)

---
+++ autopost/functions.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+
+def log(message, answer):
+    print("\n----------------------------")
+    print(datetime.now())
+    print("Сообщение от {0} {1}, (id = {2})\nТекст: {3}".format(
+        message.from_user.first_name, message.from_user.last_name,
+        str(message.from_user.id), message.text))
--- autopost/main.py
+++ autopost/main.py
@@ -3,19 +3,11 @@
 
 import telebot
 import settings
+import functions
 
 
 bot = telebot.TeleBot(settings.token)
 
-
-def log(message, answer):
-    from datetime import datetime
-    print("\n --------------------------")
-    print(datetime.now())
-    print("Сообщение от {0} {1}. (id = {2}) \n Текст - {3}".format(
-        message.from_user.first_name, message.from_user.last_name,
-        str(message.from_user.id), message.text))
-    print(answer)
 
 chat_id = settings.chat_id
 # chat_id = message.from_user_id
@@ -24,7 +16,7 @@ chat_id = settings.chat_id
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
-    user_markup.row('/start', '/stop')
+    user_markup.row('/start', '/help')
     bot.send_message(message.from_user.id, " Добрый день!\n"
                      "Отсюда можно отправлять сообщения в"
                      " канал {0}".format(chat_id),
@@ -32,25 +24,21 @@ def handle_start(message):
 
 
 @bot.message_handler(commands=['help'])
-def handle_text(message):
-    bot.send_message(message.from_user.id, "Заглушка!")
+def handle_help(message):
+    bot.send_message(message.from_user.id, "Bot's help!")
 
 
+@bot.message_handler(commands=['stop'])
+def handle_stop(message):
+    pass
+
+
+# @bot.message_hanler(content_types=['photo'])
+# def handle_photo(message):
+#     user_
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    answer = "Ты не умеешь играть в эту игру :("
-    if message.text.lower() == 'а':
-        answer = 'Б'
-        bot.send_message(chat_id, answer)
-        log(message, answer)
-    elif message.text.lower() == 'б':
-        answer = 'В'
-        bot.send_message(chat_id, answer)
-        log(message, answer)
-    elif message.text == '/stop':
-        pass
-    else:
-        bot.send_message(chat_id, answer)
-        log(message, answer)
+    bot.send_message(chat_id, message.text)
+    functions.log(message, message.text)
 
 bot.polling(none_stop=True, interval=0)
