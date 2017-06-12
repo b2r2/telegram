807ea31a32849708a50a76a202cf92f2767cc85d
b2r2 <b2r2@bk.ru>
2017-06-12 22:00:33 +0300

    working with advertising option

--- autopost/bot.py
+++ autopost/bot.py
@@ -72,71 +72,81 @@ def handle_message(message):
         handle_advertising_message(message)
         user_will_send_advertising.remove(user_id)
     else:
-        @bot.message_handler(commands=['start'])
-        def handle_start(message):
-            user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
-            user_markup.row('/start', '/help')
-            user_markup.row('/advertising')
+        pass
 
-            help_msg = """Hello!
-            \nYou can send message in channel {0}""".format(chat_id)
-            bot.send_message(message.from_user.id, help_msg,
-                             reply_markup=user_markup)
 
-        @bot.message_handler(commands=['help'])
-        def handle_help(message):
-            help_msg = """My options are very limited..."""
-            bot.send_message(message.from_user.id, help_msg)
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
+    user_markup.row('/start', '/help')
+    user_markup.row('/advertising')
 
-        @bot.message_handler(content_types=['photo'])
-        def handle_photo(message):
-            try:
-                msg_photo = message.photo[0].file_id
-                bot.send_photo(chat_id, msg_photo)
-                log_info('photo')
-            except Exception:
-                log_err('photo', chat_id)
+    help_msg = """Hello!
+    \nYou can send message in channel {0}""".format(chat_id)
+    bot.send_message(message.from_user.id, help_msg,
+                     reply_markup=user_markup)
 
-        @bot.message_handler(content_types=['sticker'])
-        def handle_sticker(message):
-            try:
-                msg_sticker = message.sticker.file_id
-                bot.send_sticker(chat_id, msg_sticker)
-                log_info('stiker')
-            except Exception:
-                log_err('stiker', chat_id)
 
-        @bot.message_handler(content_types=['audio'])
-        def handle_audio(message):
-            try:
-                msg_audio = message.audio.file_id
-                bot.send_audio(chat_id, msg_audio)
-                log_info('audio')
-            except Exception:
-                log_err('audio', chat_id)
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    help_msg = """My options are very limited..."""
+    bot.send_message(message.from_user.id, help_msg)
 
-        @bot.message_handler(content_types=['document'])
-        def handle_document(message):
-            try:
-                msg_document = message.document.file_id
-                bot.send_document(chat_id, msg_document)
-                log_info('document')
-            except Exception:
-                log_err('document', chat_id)
 
-        @bot.message_handler(content_types=['video'])
-        def handle_video(message):
-            try:
-                msg_video = message.video.file_id
-                bot.send_video(chat_id, msg_video)
-                log_info('video')
-            except Exception:
-                log_err('video', chat_id)
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    try:
+        msg_photo = message.photo[0].file_id
+        bot.send_photo(chat_id, msg_photo)
+        log_info('photo')
+    except Exception:
+        log_err('photo', chat_id)
 
-        @bot.message_handler(content_types=['text'])
-        def handle_text(message):
-            bot.send_message(chat_id, message.text)
-            log_info('text')
+
+@bot.message_handler(content_types=['sticker'])
+def handle_sticker(message):
+    try:
+        msg_sticker = message.sticker.file_id
+        bot.send_sticker(chat_id, msg_sticker)
+        log_info('stiker')
+    except Exception:
+        log_err('stiker', chat_id)
+
+
+@bot.message_handler(content_types=['audio'])
+def handle_audio(message):
+    try:
+        msg_audio = message.audio.file_id
+        bot.send_audio(chat_id, msg_audio)
+        log_info('audio')
+    except Exception:
+        log_err('audio', chat_id)
+
+
+@bot.message_handler(content_types=['document'])
+def handle_document(message):
+    try:
+        msg_document = message.document.file_id
+        bot.send_document(chat_id, msg_document)
+        log_info('document')
+    except Exception:
+        log_err('document', chat_id)
+
+
+@bot.message_handler(content_types=['video'])
+def handle_video(message):
+    try:
+        msg_video = message.video.file_id
+        bot.send_video(chat_id, msg_video)
+        log_info('video')
+    except Exception:
+        log_err('video', chat_id)
+
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.send_message(chat_id, message.text)
+    log_info('text')
 
 
 # @bot.message_handler(regexp=".*")
