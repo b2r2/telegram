d0879853e8851a6be9bc4595d989ebdfb807baee
b2r2 <b2r2@bk.ru>
2017-06-18 23:42:49 +0300

    edit def delete...

--- tkbot/utils/json_methods.py
+++ tkbot/utils/json_methods.py
@@ -15,6 +15,5 @@ def delete_double_adversiting_user_post():
     db_list = [settings.db_path, settings.copy_db_path]
     for db in db_list:
         with open(db, 'r') as db_path:
-            adv_msgs = json.load(db_path)
-            print(adv_msgs)
+            print(json.load(db_path))
             print("=========\n")
