753c98994cc45187a1c40b251515b7dc5c2db292
b2r2 <b2r2@bk.ru>
2017-06-20 22:41:10 +0300

    add unittest...

--- tkbot/utils/strings_method.py
+++ tkbot/utils/strings_method.py
@@ -1,13 +1,6 @@
 def return_concatenation_string(**advertising_messages):
-    advertising_message = ''
+    advertising_message = []
     for message in advertising_messages.values():
-        advertising_message += '\n' + str(message) + '\n'
-    return advertising_message
-
-
-if __name__ == '__main__':
-    test = {
-        'usr0': 'hello world',
-        'usr1': 'my name is...',
-    }
-    print(return_concatenation_string(**test))
+        advertising_message.append(str(message))
+        advertising_message.sort()
+    return '\n'.join(advertising_message) + '\n'
