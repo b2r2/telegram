f5d4c2483b61bcce90ce110ecd0d5600cc44725a
b2r2 <b2r2@bk.ru>
2017-07-15 22:16:19 +0300

    rename string_method -> strin.py, edit: db.py, add new method in string.py

--- tkbot/utils/strings_method.py
+++
@@ -1,6 +0,0 @@
-def returnConcatenationString(**advertising_messages):
-    advertising_message = []
-    for message in advertising_messages.values():
-        advertising_message.append(str(message))
-        advertising_message.sort()
-    return '\n'.join(advertising_message) + '\n'
