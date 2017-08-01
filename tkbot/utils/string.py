d3f26de77671be823fb23fcb469fd08126977abc
b2r2 <b2r2@bk.ru>
2017-08-01 21:55:03 +0300

    01.08.17 edit: string.py [work]

--- tkbot/utils/string.py
+++ tkbot/utils/string.py
@@ -1,18 +1,16 @@
 class String():
-    def __init__(self):
-        self.result = ''
-
     def formatting_schedule(self, sched_message):
+        result = ''
         for string in sched_message:
             if string.isdigit():
-                self.result += string.strip()
-        return self.result[:2] + ':' + self.result[-2:]
+                result += string.strip()
+        return result[:2] + ':' + result[-2:]
 
     def formatting_data(self, data):
-        for row in data[0]:
-            self.result += row + ' | '
-        print(self.result)
-        return self.result
+        result = ''
+        for column in data[0]:
+            result += column + ' | '
+        return result
 
     def get_sched_hour(self, sched_message):
         for string in sched_message:
