84e7e98d3711f45ce54c5d4fe1568beaa2288de8
b2r2 <b2r2@bk.ru>
2017-08-25 23:42:17 +0300

    25.08.17 tdd tkbot with mock: test db.py [tdd]

--- tdd/db.py
+++ tdd/db.py
@@ -6,12 +6,15 @@ class Database():
     def __init__(self):
         self.connection = sqlite3.connect(settings.db,
                                           check_same_thread=False)
-        self.connection.cursor()
+        self.create_table()
+
+    def create_table(self):
         sql = "CREATE TABLE IF NOT EXISTS USER\
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
             USER_ID INTEGER INIQUE,\
             CHANNEL TEXT,\
             MESSAGE TEXT,\
             SCHEDULE TEXT)"
-        self.connection.execute(sql)
+        cursor = self.connection.cursor()
+        cursor.execute(sql)
         self.connection.commit()
--- tdd/test_db.py
+++ tdd/test_db.py
@@ -5,16 +5,19 @@ import mock
 
 class TestcaseDatabase(unittest.TestCase):
     @mock.patch('db.sqlite3.connect')
-    def test_database_create_table_call(self, mock_sqlite3_connect):
-        mock_dbc = mock_sqlite3_connect.return_value
+    def setUp(self, mock_sqlite3_connect):
+        self.mock_connection = mock_sqlite3_connect.return_value
+        db.Database()
+
+    def test_create_table(self):
         call = "CREATE TABLE IF NOT EXISTS USER\
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
             USER_ID INTEGER INIQUE,\
             CHANNEL TEXT,\
             MESSAGE TEXT,\
             SCHEDULE TEXT)"
-        db.Database()
-        mock_dbc.execute.assert_called_with(call)
+        mock_cursor = self.mock_connection.cursor()
+        mock_cursor.execute.assert_called_with(call)
 
 
 if __name__ == '__main__':
