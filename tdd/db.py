7faf863d8681bde3ed0afe54edf35c2178e88765
b2r2 <b2r2@bk.ru>
2017-08-25 23:00:26 +0300

    25.08.17 study Mock for sqlite3 and tkbot [tdd]

--- tdd/db.py
+++ tdd/db.py
@@ -6,10 +6,11 @@ class Database():
     def __init__(self):
         self.connection = sqlite3.connect(settings.db,
                                           check_same_thread=False)
+        self.connection.cursor()
         sql = "CREATE TABLE IF NOT EXISTS USER\
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
             USER_ID INTEGER INIQUE,\
-            CHANNEL_NAME TEXT,\
+            CHANNEL TEXT,\
             MESSAGE TEXT,\
             SCHEDULE TEXT)"
         self.connection.execute(sql)
--- tdd/test_db.py
+++ tdd/test_db.py
@@ -6,16 +6,15 @@ import mock
 class TestcaseDatabase(unittest.TestCase):
     @mock.patch('db.sqlite3.connect')
     def test_database_create_table_call(self, mock_sqlite3_connect):
-        sqlite_execute_mock = mock.Mock()
-        mock_sqlite3_connect.return_value = sqlite_execute_mock
-        db.Database()
+        mock_dbc = mock_sqlite3_connect.return_value
         call = "CREATE TABLE IF NOT EXISTS USER\
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
             USER_ID INTEGER INIQUE,\
-            CHANNEL_NAME TEXT,\
+            CHANNEL TEXT,\
             MESSAGE TEXT,\
             SCHEDULE TEXT)"
-        sqlite_execute_mock.execute.assert_called_with(call)
+        db.Database()
+        mock_dbc.execute.assert_called_with(call)
 
 
 if __name__ == '__main__':
