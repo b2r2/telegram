7006cf6730b5f18782280927961e3595bf6cfc64
b2r2 <b2r2@bk.ru>
2017-08-27 21:26:19 +0300

    writed test for method add_user [tdd]

--- tdd/db.py
+++ tdd/db.py
@@ -9,7 +9,7 @@ class Database():
         self.create_table()
 
     def create_table(self):
-        sql = "CREATE TABLE IF NOT EXISTS USER\
+        sql = "CREATE TABLE IF NOT EXISTS USERS\
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
             USER_ID INTEGER INIQUE,\
             CHANNEL TEXT,\
@@ -17,4 +17,10 @@ class Database():
             SCHEDULE TEXT)"
         cursor = self.connection.cursor()
         cursor.execute(sql)
+        self.connection.commit()
+
+    def add_user(self, user_id):
+        sql = "INSERT OR IGNORE INTO USERS(USER_ID) VALUES(?)"
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (user_id,))
         self.connection.commit()
--- tdd/test_db.py
+++ tdd/test_db.py
@@ -7,17 +7,25 @@ class TestcaseDatabase(unittest.TestCase):
     @mock.patch('db.sqlite3.connect')
     def setUp(self, mock_sqlite3_connect):
         self.mock_connection = mock_sqlite3_connect.return_value
-        db.Database()
+        self.helper = db.Database()
 
     def test_create_table(self):
-        call = "CREATE TABLE IF NOT EXISTS USER\
+        call = "CREATE TABLE IF NOT EXISTS USERS\
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
             USER_ID INTEGER INIQUE,\
             CHANNEL TEXT,\
             MESSAGE TEXT,\
             SCHEDULE TEXT)"
+        self.helper
         mock_cursor = self.mock_connection.cursor()
         mock_cursor.execute.assert_called_with(call)
+
+    def test_add_user(self):
+        mock_cursor = self.mock_connection.cursor()
+        user_id = 'test'
+        self.helper.add_user(user_id)
+        call = "INSERT OR IGNORE INTO USERS(USER_ID) VALUES(?)"
+        mock_cursor.execute.assert_called_with(call, (user_id,))
 
 
 if __name__ == '__main__':
