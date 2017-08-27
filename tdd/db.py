b2545b4c84849c058676865dcb52462b591dafd7
b2r2 <b2r2@bk.ru>
2017-08-27 21:56:49 +0300

    writed test for update_channel, update_message, update_schedule and methods that onces[tdd]

--- tdd/db.py
+++ tdd/db.py
@@ -15,12 +15,45 @@ class Database():
             CHANNEL TEXT,\
             MESSAGE TEXT,\
             SCHEDULE TEXT)"
+
         cursor = self.connection.cursor()
         cursor.execute(sql)
+
         self.connection.commit()
 
     def add_user(self, user_id):
         sql = "INSERT OR IGNORE INTO USERS(USER_ID) VALUES(?)"
+
         cursor = self.connection.cursor()
         cursor.execute(sql, (user_id,))
+        self.connection.commit()
+
+    def update_channel(self, channel, user_id):
+        self.add_user(user_id)
+
+        sql = "UPDATE USERS SET CHANNEL=? WHERE USER_ID=?"
+
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (channel, user_id,))
+
+        self.connection.commit()
+
+    def update_message(self, message, user_id):
+        self.add_user(user_id)
+
+        sql = "UPDATE USERS SET MESSAGE=? WHERE USER_ID=?"
+
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (message, user_id,))
+
+        self.connection.commit()
+
+    def update_schedule(self, schedule, user_id):
+        self.add_user(user_id)
+
+        sql = "UPDATE USERS SET SCHEDULE=? WHERE USER_ID=?"
+
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (schedule, user_id,))
+
         self.connection.commit()
--- tdd/test_db.py
+++ tdd/test_db.py
@@ -10,22 +10,53 @@ class TestcaseDatabase(unittest.TestCase):
         self.helper = db.Database()
 
     def test_create_table(self):
+        mock_cursor = self.mock_connection.cursor()
+
         call = "CREATE TABLE IF NOT EXISTS USERS\
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
             USER_ID INTEGER INIQUE,\
             CHANNEL TEXT,\
             MESSAGE TEXT,\
             SCHEDULE TEXT)"
+
         self.helper
-        mock_cursor = self.mock_connection.cursor()
         mock_cursor.execute.assert_called_with(call)
 
     def test_add_user(self):
         mock_cursor = self.mock_connection.cursor()
-        user_id = 'test'
-        self.helper.add_user(user_id)
+
+        user_id = 'user_id'
         call = "INSERT OR IGNORE INTO USERS(USER_ID) VALUES(?)"
+
+        self.helper.add_user(user_id)
         mock_cursor.execute.assert_called_with(call, (user_id,))
+
+    def test_update_channel(self):
+        mock_cursor = self.mock_connection.cursor()
+
+        channel, user_id = 'channel', 'user_id'
+        call = "UPDATE USERS SET CHANNEL=? WHERE USER_ID=?"
+
+        self.helper.update_channel(channel, user_id)
+        mock_cursor.execute.assert_called_with(call, (channel, user_id,))
+
+    def test_update_message(self):
+        mock_cursor = self.mock_connection.cursor()
+
+        message, user_id = 'message', 'user_id'
+        call = "UPDATE USERS SET MESSAGE=? WHERE USER_ID=?"
+
+        self.helper.update_message(message, user_id)
+        mock_cursor.execute.assert_called_with(call, (message, user_id,))
+
+    def test_update_schedule(self):
+        mock_cursor = self.mock_connection.cursor()
+
+        schedule, user_id = 'schedule', 'user_id'
+        call = "UPDATE USERS SET SCHEDULE=? WHERE USER_ID=?"
+
+        self.helper.update_schedule(schedule, user_id)
+        mock_cursor.execute.assert_called_with(call, (schedule, user_id,))
 
 
 if __name__ == '__main__':
