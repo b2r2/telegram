8ff1704b3796eeb514507bef128f8e5708142b52
b2r2 <b2r2@bk.ru>
2017-10-28 23:27:04 +0300

    add new function and refactoring

--- feedbackbot/commands.py
+++ feedbackbot/commands.py
@@ -1,36 +1,55 @@
+import time
+
+
 class CommandsHandler():
-    def __init__(self, bot, telebot, settings):
+    def __init__(self, bot, telebot, settings,
+                 known_users, user_step):
         self.bot = bot
         self.telebot = telebot
         self.settings = settings
+        self.known_users = known_users
+        self.user_step = user_step
 
     def handle_start(self, message):
         user_markup = self.telebot.types.ReplyKeyboardMarkup(True, True)
         user_markup.row(u'О канале', u'Отзывы и предложения')
         user_markup.row(u'Условия рекламы', u'Предложить новость')
 
-        msg = (u'Доброго времени суток!\nС помощью меня Вы можете связаться '
-               'с моим создателем и администратором сообщества ' +
-               self.settings.long_link + '\nДля этого выберете одно из '
-               'возможных действий!')
-        self.bot.send_message(message.from_user.id, msg,
-                              reply_markup=user_markup)
+        cid = message.chat.id
+        if cid not in self.known_users:
+            self.known_users.append(cid)
+            self.user_step[cid] = 0
+
+            msg = (u'Доброго времени суток!\nС помощью меня Вы можете связаться '
+                   'с моим создателем и администратором сообщества ' +
+                   self.settings.long_link + '\nДля этого выберете одно из '
+                   'возможных действий!')
+            self.bot.send_message(cid, msg,
+                                  reply_markup=user_markup)
+        else:
+            self.bot.send_message(cid, "Рады, что Вы снова с нами!")
 
     def handle_about(self, message):
+        cid = message.chat.id
+
         msg = ('Проект создан для программистов любого уровня.\n'
                'Здесь Вы всегда можете узнать что-то новое!\n\n'
                'С помощью меня Вы можете связаться с моим создателем и '
                'администратором сообщества ' + self.settings.short_link +
                '\nДля этого выберете одно из возможных действий из '
                'контекстного меню.')
-        self.bot.send_message(message.from_user.id, msg)
+        self.bot.send_message(cid, msg)
 
     def handle_feedback(self, message):
+        cid = message.chat.id
+
         msg = ('Вы можете оставить свои отзывы о канале и пожелания.\n'
                'Мы стараемся для Вас!')
-        self.bot.send_message(message.from_user.id, msg)
+        self.bot.send_message(cid, msg)
 
     def handle_advertising(self, message):
+        cid = message.chat.id
+
         msg = ('Условия рекламы на канале ' + self.settings.short_link +
                '\n\nОтдельный пост: 1000 рублей, выход поста 14:00\n'
                'Пост в подборке: 500 рублей, выход поста в 20:00\n\n'
@@ -40,8 +59,28 @@ class CommandsHandler():
                'От 3 постов - 5%\n'
                'От 5 постов - 7%\n'
                'От 10 постов - 10%\n')
-        self.bot.send_message(message.from_user.id, msg)
+        self.bot.send_message(cid, msg)
 
     def handle_suggest(self, message):
+        cid = message.chat.id
+
         msg = 'Здесь Вы можете поделиться интересными фактами и событиями.'
-        self.bot.send_message(message.from_user.id, msg)
+        self.bot.send_message(cid, msg)
+
+    def handle_text(self, message):
+        cid = message.chat.id
+        mid = message.message_id
+
+        smiley = u'\U0001F609'
+        msg = "Ваше сообщение отправлено!\nСпасибо! " + smiley
+        self.bot.send_message(cid, msg)
+
+        self.bot.forward_message(chat_id=self.settings.target_chat,
+                                 from_chat_id=cid,
+                                 message_id=mid)
+
+    def handle_long_text(message):
+        cid = message.chat.id
+        self.send_chat_action(cid, 'typing')
+        time.sleep(3)
+        bot.send_message(cid, '.')
--- feedbackbot/main.py
+++ feedbackbot/main.py
@@ -6,36 +6,69 @@ import commands
 import settings
 
 
+known_users = []
+user_step = {}
+
+
+def get_user_step(uid):
+    if uid in user_step:
+        return user_step[uid]
+    else:
+        known_users.append[uid]
+        user_step[uid] = 0
+        print("New user detected, who hasn't user \'start' yet")
+        return 0
+
+
+def listener(message):
+    """
+    When new message arrive Bot will call this function.
+    """
+    for m in message:
+        if m.content_type == 'text':
+            # print the sent message to the console
+            print(str(m.chat.first_name) + ' [' + str(m.chat.id) +
+                  ']: ' + m.text)
+
+
 bot = telebot.TeleBot(settings.token)
-commands = commands.CommandsHandler(bot, telebot, settings)
+bot.set_update_listener(listener)  # register listener
+
+commands = commands.CommandsHandler(bot, telebot, settings,
+                                    known_users, user_step)
 
 
 @bot.message_handler(commands=['start'])
-def handle_start(message):
+def command_start(message):
     commands.handle_start(message)
 
 
 @bot.message_handler(commands=['about'])
-def handle_about(message):
+def command_about(message):
     commands.handle_about(message)
 
 
 @bot.message_handler(commands=['feedback'])
-def handle_feedback(message):
+def command_feedback(message):
     commands.handle_feedback(message)
 
 
 @bot.message_handler(commands=['advertising'])
-def handle_advertising(message):
+def command_advertising(message):
     commands.handle_advertising(message)
 
 
 @bot.message_handler(commands=['suggest'])
-def handle_suggest(message):
+def command_suggest(message):
     commands.handle_suggest(message)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['sendLongText'])
+def command_long_text(message):
+    commands.handle_long_text(message)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def handle_text(message):
     if message.text == 'О канале':
         commands.handle_about(message)
@@ -46,12 +79,7 @@ def handle_text(message):
     elif message.text == 'Предложить новость':
         commands.handle_suggest(message)
     else:
-        bot.forward_message(chat_id=settings.target_chat,
-                            from_chat_id=message.chat.id,
-                            message_id=message.message_id)
-        smiley = u'\U0001F609'
-        msg = 'Ваше сообщение отправлено!\nВсего доброго ' + smiley
-        bot.send_message(message.from_user.id, msg)
+        commands.handle_text(message)
 
 
 if __name__ == '__main__':
