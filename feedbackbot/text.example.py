ddbca269d4e9e3f883042a1a1f7648692fb3de27
b2r2 <b2r2@bk.ru>
2017-11-26 23:21:55 +0300

    edit text.example.py

--- feedbackbot/text.example.py
+++ feedbackbot/text.example.py
@@ -1,4 +1,4 @@
 SMILEY = u'\U0001F609'
 IGNORE = 'some about ignore message'
 DEFAULT = 'any default message for user'
-EMPTY = 'some empty message for admin'
+MISTAKE = 'some mistake message for admin'
