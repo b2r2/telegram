547436aac1658c504efa068d4dcc361377c304ae
b2r2 <b2r2@bk.ru>
2018-01-22 23:00:41 +0300

    Refactoring

--- feedbackbot/main.py
+++ feedbackbot/main.py
@@ -6,6 +6,8 @@ import telebot
 import handler
 import utils
 from config import TOKEN
+from config import IGNORE_TYPES
+from config import CONTENT_TYPES
 
 
 bot = telebot.AsyncTeleBot(TOKEN)
@@ -20,12 +22,21 @@ def sending_command(message):
     handler.send_command(message)
 
 
-@bot.message_handler(func=lambda message: message.chat.type in 'supergroup')
+@bot.message_handler(func=lambda message: message.chat.type in 'supergroup',
+                     content_types=['text', 'photo', 'sticker'])
 def sending_message(message):
-    handler.send_message(message)
+    handler.sending(message)
 
 
-@bot.message_handler(func=lambda message: message.chat.type in 'private')
+@bot.message_handler(func=lambda message: message.chat.type in 'supergroup',
+                     content_types=IGNORE_TYPES)
+def sending_error(message):
+    handler.send_error(message.chat.id,
+                       'Please sending only text, photo or sticker')
+
+
+@bot.message_handler(func=lambda message: message.chat.type in 'private',
+                     content_types=CONTENT_TYPES)
 def forwarding_message(message):
     handler.forward_message(message)
 
